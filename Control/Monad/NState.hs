b16f077c66fed7aa639253ddbea9fd355d793d9c
Stuart A. Kurtz <stuart@cs.uchicago.edu>
2013-11-09 15:18:13 -0600

    0.1.0.0

---
+++ Control/Monad/NState.hs
@@ -0,0 +1,53 @@
+{- |
+  Module      :  Control.Monad.NState
+  Copyright   :  (c) 2013, Stuart A. Kurtz
+  License     :  BSD (3-Clause) (see ./LICENSE)
+  
+  Maintainer  :  stuart@cs.uchicago.edu
+  Stability   :  experimental
+ 
+  A nondeterministic state monad.
+-}
+
+module Control.Monad.NState(NState,get,put,state,modify,run,eval,exec,branch) where
+
+import Control.Monad.State
+
+type NState s a = StateT s [] a
+
+{- |
+	Unwrap an NState monad computation as a function.
+-}
+
+run :: NState s a  -- ^ nondeterministic state computation
+	-> s		   -- ^ initial state
+	-> [(a,s)]     -- ^ list of return values and final states
+run = runStateT
+
+{- |
+  Evaluate a non-deterministic state computation with the given initial
+  state and return the list of final values, discarding the final states.
+-}
+
+eval :: NState s a  -- ^ nondeterministic state computation
+	 -> s		    -- ^ initial state
+	 -> [a]         -- ^ list of return values
+eval = evalStateT
+
+{- |
+  Evaluate a non-deterministic state computation with the given initial
+  state and return the list of final states, discarding the final vailes.
+-}
+
+exec :: NState s a  -- ^ nondeterministic state computation
+	 -> s		    -- ^ initial state
+	 -> [s]         -- ^ list of return states
+exec = execStateT
+
+{- |
+  Non-deterministically select a value from the argument list.
+-}
+
+branch :: [a]            -- ^ list of alternatives
+          -> NState s a  -- ^ nondeterministic state computation
+branch = lift
