953a0ab017e37aefa67174939381a46bbcce7aaf
Stuart A. Kurtz <stuart@cs.uchicago.edu>
2013-11-09 17:08:41 -0600

    0.2.0.0

--- Control/Monad/NState.hs
+++ Control/Monad/NState.hs
@@ -9,7 +9,18 @@
   A nondeterministic state monad.
 -}
 
-module Control.Monad.NState(NState,get,put,state,modify,run,eval,exec,branch) where
+module Control.Monad.NState
+    ( NState
+    , Control.Monad.NState.get
+    , Control.Monad.NState.put
+    , Control.Monad.NState.state
+    , nstate
+    , Control.Monad.NState.modify
+    , branch
+    , runNState
+    , evalNState
+    , execNState
+    ) where
 
 import Control.Monad.State
 
@@ -20,38 +31,75 @@ import Control.Monad.State
 type NState s = StateT s []
 
 {- |
-	Unwrap an "NState" computation as a function.
+  Return the state from the internals of the monad.
 -}
 
-run :: NState s a  -- ^ nondeterministic state computation
-	-> s		   -- ^ initial state
-	-> [(a,s)]     -- ^ list of return values and final states
-run = runStateT
+get :: NState s s
+get = Control.Monad.State.get
+
+{- |
+  Replace the state inside the monad.
+-}
+
+put :: s -> NState s ()
+put = Control.Monad.State.put
+
+{- |
+  Embed a simple state action into the monad.
+-}
+
+state :: (s -> (a,s)) -> NState s a
+state = Control.Monad.State.state
+
+{- |
+  Embed a non-deterministic function state function, realized via the type
+  @s -> [(a,s)]@, into the "NState" monad.
+-}
+
+nstate :: (s -> [(a,s)]) -> NState s a
+nstate = StateT
+
+{- |
+  Maps an old state to a new state inside a state monad. The old state is thrown away.
+-}
+
+modify :: (s -> s) -> NState s ()
+modify = Control.Monad.State.modify
+
+{- |
+  Non-deterministically select a value from the list of alternatives.
+-}
+
+branch :: [a]            -- ^ list of alternatives
+          -> NState s a  -- ^ nondeterministic state computation
+branch = lift
+
+{- |
+    Unwrap an "NState" computation as a function.
+-}
+
+runNState :: NState s a  -- ^ nondeterministic state computation
+          -> s          -- ^ initial state
+          -> [(a,s)]    -- ^ list of return values and final states
+runNState = runStateT
+
 
 {- |
   Evaluate a non-deterministic state computation with the given initial
   state and return the list of final values, discarding the final states.
 -}
 
-eval :: NState s a  -- ^ nondeterministic state computation
-	 -> s		    -- ^ initial state
-	 -> [a]         -- ^ list of return values
-eval = evalStateT
+evalNState :: NState s a  -- ^ nondeterministic state computation
+           -> s           -- ^ initial state
+           -> [a]         -- ^ list of return values
+evalNState = evalStateT
 
 {- |
   Evaluate a non-deterministic state computation with the given initial
   state and return the list of final states, discarding the final vailes.
 -}
 
-exec :: NState s a  -- ^ nondeterministic state computation
-	 -> s		    -- ^ initial state
-	 -> [s]         -- ^ list of return states
-exec = execStateT
-
-{- |
-  Non-deterministically select a value from the argument list.
--}
-
-branch :: [a]            -- ^ list of alternatives
-          -> NState s a  -- ^ nondeterministic state computation
-branch = lift
+execNState :: NState s a  -- ^ nondeterministic state computation
+           -> s           -- ^ initial state
+           -> [s]         -- ^ list of return states
+execNState = execStateT
